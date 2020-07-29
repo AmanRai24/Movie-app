d64698b63b2ea041181bcdd7ebe6b5a5b4be1499
Aman Rai <arai24197@gmail.com>
2020-07-29 20:50:49 +0530

    change state to add fav

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { data } from '../data';
 import Navbar from "./Navbar";
 import MovieCard from "./MovieCard";
+import { addMovies } from "../actions";
 
 class App extends React.Component {
 
@@ -12,20 +13,18 @@ class App extends React.Component {
           //suscribe call will automatically call when we dispatch
           store.subscribe(() => {
           console.log('UPDATE');
-          this.forceUpdate(); //--- forceupdate is not a good way of doing
+          this.forceUpdate(); //forceupdate is not a good way of doing
         })
 
           //dispatch action
-          store.dispatch({
-          type: 'ADD_MOVIES',
-          movies: data
-        });
+          store.dispatch(addMovies(data));
 
         console.log('STATE:',store.getState());
   }
   render() {
 
-    const movies = this.props.store.getState();
+    //const movies = this.props.store.getState();
+    const { list } = this.props.store.getState();
     console.log("RENDER");
     return (
       <div className="App" >
@@ -40,7 +39,7 @@ class App extends React.Component {
 
       <div className="list">
         {/* map send every (element, index of element) in array  */}
-        { movies.map( (movie,index) =>(
+        { list.map( (movie,index) =>(
         <MovieCard movie ={movie} key={`movies-${index}`} />
         ))}
       </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,19 @@
+import { ADD_MOVIES } from '../actions';
 //if action contains new state so we return that state
 //if no change in state so we return curr-state 
-export default function movies(state=[],action){
-    if(action.type==='ADD_MOVIES'){
-        return action.movies;
+// export default function movies(state=[],action){
+//     if(action.type==='ADD_MOVIES'){
+//         return action.movies;
+const initialMovieState = {
+    list: [],
+    favourites: []
+}
+export default function movies (state = initialMovieState, action) {
+    if(action.type === ADD_MOVIES) {
+        return {
+            ...state, //spread operation on obj (add all state data i.e (list,fav))
+            list: action.movies  //(change list )
+        }
     }
     return state;
 }
