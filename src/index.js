c98a867f9e714cd45fdc50920b29113b3125699a
Aman Rai <arai24197@gmail.com>
2020-07-30 21:05:02 +0530

    update middleware

--- src/index.js
+++ src/index.js
@@ -6,16 +6,21 @@ import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
-// function logger(obj,next,action)
-// logger(obj)(next)(action)
-// obj ={dispatch, getState}
-const logger = function({ dispatch, getState}){
-    return function(next){
-        return function(action){
-            console.log("ACTION_TYPE:",action.type);
-            next(action); //pass to another MW if present else go to dispatch
-        }
-    }
+// using curring
+
+    // const logger = function({ dispatch, getState}){
+    //     return function(next){
+    //         return function(action){
+    //             console.log("ACTION_TYPE:",action.type);
+    //             next(action); //-- pass to another MW if present else go to dispatch
+    //         }
+    //     }
+    // }
+
+// diff to call function
+const logger = ({ dispatch, getState}) => (next) => (action) => {
+    console.log("ACTION_TYPE:",action.type);
+    next(action); // pass to another MW if present else go to dispatch
 }
 
 const store = createStore(rootReducer, applyMiddleware(logger));
